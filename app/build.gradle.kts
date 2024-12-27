47d44f25cc108dfd0939636f10b55a1dcc4fb4b3
Cristian Morales <crisdavidmm95.dev@gmail.com>
2024-12-26 21:36:37 -0500

    first version [needs corrections]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.practicecomposebasic"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.practicecomposebasic"
--- app/src/main/java/com/example/practicecomposebasic/MainActivity.kt
+++ app/src/main/java/com/example/practicecomposebasic/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practicecomposebasic.ui.theme.PracticeComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +28,64 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticeComposeBasicTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PracticeCompose(
+                    stringResource(R.string.title),
+                    stringResource(R.string.first_text),
+                    stringResource(R.string.second_text),
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
             }
         }
     }                                  
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PracticeCompose(title: String, firstText: String, secondText: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+        ParagraphText(
+            title = title,
+            firstText = firstText,
+            secondText = secondText,
+        )
+    }
+}
+
+@Composable
+fun ParagraphText(title: String, firstText: String, secondText: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.
+            padding(16.dp)
+    ) {
+
+        Text(
+            text = title,
+            fontSize = 24.sp,
+        )
+        Text(
+            text = firstText,
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = secondText,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PracticeComposePreview() {
     PracticeComposeBasicTheme {
-        Greeting("Android")
+        PracticeCompose(
+            stringResource(R.string.title),
+            stringResource(R.string.first_text),
+            stringResource(R.string.second_text)
+        )
     }
 }
